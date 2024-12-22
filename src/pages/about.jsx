81f89c95db3d25da5165aff284f970799b721c0d
M Sri Lalith <msrilalith30@gmail.com>
2024-12-22 19:41:25 +0530

    photo update

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -17,7 +17,8 @@ import Aditya from '../assets/memberImages/Aditya.jpeg'
 import Sanath from '../assets/memberImages/Sanath.jpg'
 import Atharva from '../assets/memberImages/Atharva.jpg'
 import Sanchit from '../assets/memberImages/Sanchit_Nigam.jpeg'
-
+import Arya from '../assets/memberImages/AryaP.jpeg'
+import Lalith from '../assets/memberImages/Lalith.jpeg'
 export default function about() {
   return (
     <div>
@@ -31,9 +32,9 @@ export default function about() {
             <TeamCards name="Kushagra Sharma" img={Harsh} post="President" data="Computer Science and Engineering" linkedin={"https://www.linkedin.com/in/harsh-agarwal-364439228/"} phone="917549395833" twitter={"https://twitter.com/HK_Agrwl"} github={"https://github.com/HKAgrwl"} />
             <TeamCards name="Pragya Gaur" img={Sanath} post="Vice President" data="Computer Science and Engineering" />
             <TeamCards name="Abhijeet Sharma" img={Aashray} post="Android Lead" data="Computer Science and Engineering" linkedin={"https://www.linkedin.com/in/aashray-gupta-26a503246/"} phone={"7009378912"} />
-            <TeamCards name="Arya Putatunda" img={ArjavBarya} post="AI & ML Lead" data="Mechanical Engineering" linkedin={"https://www.linkedin.com/in/arjav-barya-569584235/"} phone={"9301763998"} />
+            <TeamCards name="Arya Putatunda" img={Arya} post="AI & ML Lead" data="Computer Science and Engineering" linkedin={"https://www.linkedin.com/in/arya-putatunda-002b40261/"} github={"https://github.com/Movazed?tab=repositories"} instagram={""} />
             <TeamCards name="Hitesh Bajaj" img={AravJain} post="Socail Lead I" data="Computer Science and Engineering" linkedin={"https://www.linkedin.com/in/aaravjn/"} phone="7487853240" />
-            <TeamCards name="Lalith Mulukutla" img={Prakhar} post="Web Dev Lead" data="Computer Science and Engineering" linkedin={"https://www.linkedin.com/in/msrilalith/"} github={"https://github.com/mlalith0302"} instagram={"https://www.instagram.com/lalith0302/"} />
+            <TeamCards name="Lalith Mulukutla" img={Lalith} post="Web Dev Lead" data="Computer Science and Engineering" linkedin={"https://www.linkedin.com/in/msrilalith/"} github={"https://github.com/mlalith0302"} instagram={"https://www.instagram.com/lalith0302/"} />
             <TeamCards name="Sanchit Nigam" img={Sanchit} post="Competitive Programming Lead" data="Computer Science and Engineering" linkedin={"https://www.linkedin.com/in/sanchit-nigam-6a899a30a/"} github={"https://github.com/sanchit-cmd"} instagram={" https://www.instagram.com/nigam.sanchit/"}/>
             <TeamCards name="Zuhaib Farooqui" img={Vishwaroop} post="Soical Media Lead II" data="Chemical Engineering" linkedin={"https://www.linkedin.com/in/vishwaroop-gangarde-118a02228/"} phone="7709888143" />
             
