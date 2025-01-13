f82547e169c06bf68a3c550ec96cb9be9f3789a6
Pragya Gaur <gaur273pragya@gmail.com>
2025-01-14 00:25:00 +0530

    Styling changes of Register Page

--- src/pages/register.jsx
+++ src/pages/register.jsx
@@ -1,12 +1,19 @@
-import React from 'react';
+import React from "react";
 
 const Register = () => {
   return (
-    <div className="flex justify-center items-center min-h-screen bg-gradient-to-br from-gray-300 to-gray-600">
-      <div className=" shadow-md rounded-lg overflow-hidden w-full max-w-3xl">
+    <div className="flex justify-center items-start min-h-screen bg-[#110f16] pt-20">
+      <div className="shadow-md rounded-lg overflow-hidden w-full max-w-3xl">
         <div className="p-6">
           <div className="overflow-hidden rounded-lg">
-            <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSfPCJ-7UU4flpul9ZBchu-5caJdvLe74DKbQu0VOGaDynlUHw/viewform?embedded=true" width="640" height="900" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
+            <iframe
+              src="https://docs.google.com/forms/d/e/1FAIpQLSfPCJ-7UU4flpul9ZBchu-5caJdvLe74DKbQu0VOGaDynlUHw/viewform?embedded=true"
+              width="640"
+              height="900"
+              className="w-full"
+            >
+              Loading…
+            </iframe>
           </div>
         </div>
       </div>
